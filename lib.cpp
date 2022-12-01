a07d8c34e83bc47ca967362216c62bcc69b91ee1
FrancescoBottaro <117718788+FrancescoBottaro@users.noreply.github.com>
2022-12-02 00:02:40 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+bool num_prim(int n, int i)
+{
+    int r;
+    if (i < 2)
+        return true;
+    r = n % i;
+    if (r == 0)
+        return false;
+    else
+        r = num_prim(n, i - 1);
+}
